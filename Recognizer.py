709aa7b439c0e378a6860ab74525292cdea559fa
SoulPlayer23 <venkiteshsanand1920@gmail.com>
2021-05-15 11:07:03 +0530

    Keyword Trigger

--- Recognizer.py
+++ Recognizer.py
@@ -3,12 +3,16 @@ import speech_recognition as sr
 r = sr.Recognizer()
 m = sr.Microphone()
 
+keywords = ["Venkatesh"]
 #Assigning microphone as source
 with m:
     print("Speak anything : ")
     #Listening to audio from microphone
-    audio = r.listen(m, 5)
+    audio = r.listen(m, timeout = 5)
     #Converting audio to text
     text = r.recognize_google(audio)
     print("You said:", text)
+    for i in keywords:
+        if i in text:
+            print("triggered")
     
